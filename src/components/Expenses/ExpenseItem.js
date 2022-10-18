d15803bae2cb9dc4c4451364ff5ba05664e5de4d
Venkat Gadiyakari <venkat>
2022-10-18 15:07:51 +0530

    introduced the concept of state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
+
 function ExpenseItem(props) {
+
+  //must be called directly under the function.
+  //return value and a modifying function.
+  //first value is current state value
+  //second value is a function for updating that value.
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated");
+  }
+ 
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
